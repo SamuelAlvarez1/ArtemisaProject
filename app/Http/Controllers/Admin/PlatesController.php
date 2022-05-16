7a5fd295d8a864f3c6c28f8ba02025920b2edc96
SGZ34 <simongiraldo34@gmail.com>
2022-05-16 18:45:06 -0500

    commit

--- app/Http/Controllers/Admin/PlatesController.php
+++ app/Http/Controllers/Admin/PlatesController.php
@@ -59,43 +59,16 @@ class PlatesController extends Controller
     public function store(Request $request)
     {
 
-         $request->validate(Plate::$rules);
+        $request->validate(Plate::$rules);
         $input = $request->all();
         try {
+            Plate::create([
+                "name" => $input["name"],
+                "price" => $input["price"],
+                "idCategory" => $input["idCategory"],
+                "state" => 1
+            ]);
 
-<<<<<<< HEAD
-                $plates = Plate::all();
-                $namePlates = [];
-                foreach ($input["id"] as $key => $value) {
-
-                    $plates = Plate::where('name', $input["plate"][$key])->first();
-                    if ($plates) {
-                        $namePlates[sizeof($namePlates)] = $input["plate"][$key];
-                    } else {
-                        Plate::create([
-                            "name" => $input["plate"][$key],
-                            "price" => $input["prices"][$key],
-                            "idCategory" => $input["categories"][$key],
-                            "state" => 1
-                        ]);
-                    }
-                }
-            }
-
-            DB::commit();
-            if (sizeof($namePlates) > 0) {
-
-                return redirect('/plates')->with(['nameDuplicate' => $namePlates]);
-            }
-=======
-                        Plate::create([
-                        "name" => $input["name"],
-                        "price" => $input["price"],
-                        "idCategory" => $input["idCategory"],
-                        "state" => 1
-                    ]);
-
->>>>>>> 0d43536de06425a0ed5a2430b73a4f7ce2c5a93c
             return redirect('/plates')->with('success', 'Platillo creado exitosamente');
         } catch (\Exception $e) {
             DB::rollBack();
@@ -107,13 +80,8 @@ class PlatesController extends Controller
     public function show($id)
     {
         $plates = Plate::find($id);
-<<<<<<< HEAD
-
-        return view('plates.show', compact('plates'));
-=======
         $platesSalesCount = SaleDetail::where('idPlate', $id)->sum('quantity'); //cantidad de ventas del platillo
         return view('plates.show', compact('plates', 'platesSalesCount'));
->>>>>>> 0d43536de06425a0ed5a2430b73a4f7ce2c5a93c
     }
 
 
@@ -132,39 +100,26 @@ class PlatesController extends Controller
 
     public function update(Request $request, $id)
     {
-<<<<<<< HEAD
-        if ($id != null) {
-            $name = Plate::where('name', $request["plate"]);
-            if ($name) {
-                return redirect('/plates')->with("error", "El nombre al que quiere actualizar el platillo ya existe");
-            }
-=======
         $validator = Validator::make($request->all(), Plate::$rulesEdit);
-        $validator->after(function ($validator) use ($request, $id){
-            $plate = Plate::where('name', $request->input('name'))->where('id','!=', $id)->first();
+        $validator->after(function ($validator) use ($request, $id) {
+            $plate = Plate::where('name', $request->input('name'))->where('id', '!=', $id)->first();
             if ($plate)
                 $validator->errors()->add('name', 'Este nombre ya está en uso');
         });
-        if ($validator->fails()){
+        if ($validator->fails()) {
             return back()->withErrors($validator)->withInput();
         }
->>>>>>> 0d43536de06425a0ed5a2430b73a4f7ce2c5a93c
-            try {
+        try {
 
-                Plate::where("id", $id)->update([
-                    "name" => $request["name"],
-                    "price" => $request["price"],
-<<<<<<< HEAD
-                    "idCategory" => $request["category"]
-=======
-                    "idCategory" => $request["idCategory"]
->>>>>>> 0d43536de06425a0ed5a2430b73a4f7ce2c5a93c
-                ]);
-                return redirect('/plates')->with("success", "El platillo fue editado satisfactoriamente");
-            } catch (\Exception $e) {
-                return redirect('/plates')->with("error", $e->getMessage());
-            }
-
+            Plate::where("id", $id)->update([
+                "name" => $request["name"],
+                "price" => $request["price"],
+                "idCategory" => $request["idCategory"]
+            ]);
+            return redirect('/plates')->with("success", "El platillo fue editado satisfactoriamente");
+        } catch (\Exception $e) {
+            return redirect('/plates')->with("error", $e->getMessage());
+        }
     }
 
     public function destroy($id)
@@ -177,5 +132,4 @@ class PlatesController extends Controller
         $price = Plate::select("plates.price")->where("id", $id)->first();
         return $price;
     }
-
 }
